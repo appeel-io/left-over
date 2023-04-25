69b586c9dfb92f10115887920379bb101a626d57
Janny Bomans <janny.bomans@appeel.io>
2023-04-25 10:34:00 +0200

    feat(get-page): split sidebar into seperate component

---
+++ components/organisms/get/Sidebar.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { useCategoriesStore } from '@/store/categories'
+import { useAllergiesStore } from '@/store/allergies'
+
+const categoriesStore = useCategoriesStore()
+const allergiesStore = useAllergiesStore()
+</script>
+
+<template>
+  <div class="flex flex-col">
+    <div>
+      <p v-for="category in categoriesStore.data" :key="category.id">
+        {{ category.label }}
+      </p>
+    </div>
+    <div>
+      <p v-for="category in allergiesStore.data" :key="category.id">
+        {{ category.label }}
+      </p>
+    </div>
+  </div>
+</template>
--- pages/get.vue
+++ pages/get.vue
@@ -1,28 +1,15 @@
 <script setup>
 import { usePostingsStore } from '@/store/postings'
-import { useCategoriesStore } from '@/store/categories'
-import { useAllergiesStore } from '@/store/allergies'
 
 const postingsStore = usePostingsStore()
-const categoriesStore = useCategoriesStore()
-const allergiesStore = useAllergiesStore()
+
 </script>
 
 <template>
   <NuxtLayout>
     <section class="grid grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 min-h-[85vh]">
-      <div class="flex flex-col">
-        <div>
-          <p v-for="category in categoriesStore.data" :key="category.id">
-            {{ category.label }}
-          </p>
-        </div>
-        <div>
-          <p v-for="category in allergiesStore.data" :key="category.id">
-            {{ category.label }}
-          </p>
-        </div>
-      </div>
+      <GetSidebar />
+
       <div class="col-span-2 lg:col-span-3 xl:col-span-4">
         <div v-if="postingsStore.data" class="grid">
           <FoodItemsList :food-items="postingsStore.data" />
