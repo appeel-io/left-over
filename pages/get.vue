86959bda30333eac411f009c19675d1ef9db02e6
Jeremy <jeremy.mees@appeel.io>
2023-04-25 11:53:08 +0200

    chore(page): merged pages

--- pages/get.vue
+++ pages/get.vue
@@ -7,9 +7,9 @@ const postingsStore = usePostingsStore()
 
 <template>
   <NuxtLayout>
-    <section class="grid grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 min-h-[85vh] py-4">
+    <section class="grid grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 min-h-[85vh]">
       <GetSidebar />
-      <div class="col-span-2 lg:col-span-3 xl:col-span-4">
+      <div class="col-span-2 lg:col-span-3 xl:col-span-4 py-4">
         <div v-if="postingsStore.data" class="flex flex-col gap-2">
           <FoodItemsList :food-items="postingsStore.data" class="mx-6" />
           <Map
